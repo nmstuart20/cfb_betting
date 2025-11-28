387a5ecdab127b75668b3c3c85ecce6e8d077c8f
nickyflash <nmstuart20@gmail.com>
2025-11-27 22:39:27 -0600

    ignore fetch for now to save api keys on push

--- src/api/game_results_api.rs
+++ src/api/game_results_api.rs
@@ -161,6 +161,7 @@ mod tests {
     use chrono::{Datelike, Local};
 
     #[tokio::test]
+    #[ignore]
     async fn test_fetch_games() {
         dotenv::dotenv().ok();
         let api_key = std::env::var("COLLEGE_FOOTBALL_DATA_API_KEY")
